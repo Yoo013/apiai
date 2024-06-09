59e5a98fbf60d6fe3e50d4f8e882b39621b0a65b
Yoo013 <alfraka744@gmail.com>
2024-06-09 15:44:37 +0700

    sabl

--- index.js
+++ index.js
@@ -14,10 +14,12 @@ const app = express()
 
 app.use(express.json())
 app.use(cors({
-    origin: ["https://frontend-v4fu.vercel.app"],
-    methods: ["GET", "POST"],
-    credentials: true
-}))
+    origin: "https://apiai-apii.vercel.app",
+    headers: {
+        "Access-Control-Allow-Origin": "https://apiai-apii.vercel.app",
+        "Access-Control-Allow-Credentials": true
+    },
+}));
 app.use(cookieParser())
 app.use('/auth', UserRouter)
 
