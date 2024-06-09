43bbcf571a4eb34d68964618dccd7607e2239ba2
Yoo013 <alfraka744@gmail.com>
2024-06-09 14:56:51 +0700

    sabi

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const app = express()
 
 app.use(express.json())
 app.use(cors({
-    origin: ["https://frontend-3a5m.vercel.app"],
+    origin: ["https://frontend-v4fu.vercel.app"],
     methods: ["GET", "POST"],
     credentials: true
 }))
